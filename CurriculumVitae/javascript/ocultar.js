1f34251f72b40fe5f909092e84425df4c1675722
Sergio Fradin <fradinsergio@gmail.com>
2023-09-13 10:15:22 -0300

    Primer escrritura en el repositorio

---
+++ CurriculumVitae/javascript/ocultar.js
@@ -0,0 +1,4 @@
+document.getElementById('bn_ocultar').onclick = function(){
+    document.getElementById('hobbies').style.display="none"
+    document.getElementById('pie').style.marginTop="320px"
+}
